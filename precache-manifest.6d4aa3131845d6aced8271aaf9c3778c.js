e9f42d549de22de05081c991ed51009a2884a25d
Anovikov <dang3rouz@yandex.ru>
2019-12-24 19:22:16 +0500

    Updates

---
+++ precache-manifest.6d4aa3131845d6aced8271aaf9c3778c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4381e6a8bb5c2243f6120fea26c10b80",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0c578efbe24c9763a192",
+    "url": "/monsters-rolodex/static/css/main.fe5b95f9.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0c578efbe24c9763a192",
+    "url": "/monsters-rolodex/static/js/main.295c723c.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
